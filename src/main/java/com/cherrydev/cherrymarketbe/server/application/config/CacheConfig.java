e05dedd8c3f9c12376f4f13fc776c28340a36116
SeongHo5 <newfuring1123@gmail.com>
2024-02-18 22:22:37 +0900

    feat: Update cache configs
    - 캐시 직렬화 설정 추가

--- src/main/java/com/cherrydev/cherrymarketbe/server/application/config/CacheConfig.java
+++ src/main/java/com/cherrydev/cherrymarketbe/server/application/config/CacheConfig.java
@@ -8,6 +8,9 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
@@ -17,12 +20,24 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // 키 직렬화 설정
+        RedisSerializationContext.SerializationPair<String> keySerialization =
+                RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer());
+
+        // 값 직렬화 설정
+        RedisSerializationContext.SerializationPair<Object> valueSerialization =
+                RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+
         RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(30))
+                .serializeKeysWith(keySerialization)
+                .serializeValuesWith(valueSerialization)
                 .disableCachingNullValues();
 
         RedisCacheConfiguration accountCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
+                .serializeKeysWith(keySerialization)
+                .serializeValuesWith(valueSerialization)
                 .disableCachingNullValues();
 
         return RedisCacheManager.builder()
