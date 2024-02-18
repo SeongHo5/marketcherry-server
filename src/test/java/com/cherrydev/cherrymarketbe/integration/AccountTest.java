2a5bfd6b0c46ca6dbfed74f7595d696f37b72611
SeongHo5 <newfuring1123@gmail.com>
2024-02-18 18:03:10 +0900

    test: Add account register integration test
    - 회원 가입 통합 테스트 추가

---
+++ src/test/java/com/cherrydev/cherrymarketbe/integration/AccountTest.java
@@ -0,0 +1,52 @@
+package com.cherrydev.cherrymarketbe.integration;
+
+import com.cherrydev.cherrymarketbe.server.domain.account.dto.request.RequestSignUp;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.aot.DisabledInAotMode;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.stream.Stream;
+
+import static com.cherrydev.cherrymarketbe.integration.TestHelper.convertToJSONString;
+import static com.cherrydev.cherrymarketbe.integration.factory.AccountFactory.createSignUpRequestDtoA;
+import static com.cherrydev.cherrymarketbe.integration.factory.AccountFactory.createSignUpRequestDtoB;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Rollback
+@Transactional
+@SpringBootTest
+@DisabledInAotMode
+@AutoConfigureMockMvc
+class AccountTest {
+
+    @Autowired
+    private TestHelper testHelper;
+
+    @TestFactory
+    Stream<DynamicTest> dynamicTestForRegister() throws JsonProcessingException {
+        // Given
+        // When & Then
+        return Stream.of(
+                testHelper.testApiEndpoint("회원가입 성공", registerAccount(createSignUpRequestDtoA()), status().is2xxSuccessful()),
+                testHelper.testApiEndpoint("회원가입 실패 - 이미 가입된 이메일", registerAccount(createSignUpRequestDtoA()), status().is4xxClientError()),
+                testHelper.testApiEndpoint("회원가입 실패 - 금지된 이름(관리자, 운영자 등)", registerAccount(createSignUpRequestDtoB()), status().is4xxClientError())
+
+        );
+    }
+
+    private RequestBuilder registerAccount(RequestSignUp request) throws JsonProcessingException {
+        return post("/api/accounts")
+                .contentType(APPLICATION_JSON)
+                .content(convertToJSONString(request));
+    }
+}
