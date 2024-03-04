6386498daa47190c89a63cb4ddc5252a3ac0a848
SeongHo5 <newfuring1123@gmail.com>
2024-03-04 19:40:44 +0900

    style: Move EnableJpaAuditing annotation
    - EnableJpaAuditing를 메인 클래스에서 JpaConfig 클래스로 이동

--- src/main/java/com/cherrydev/cherrymarketbe/CherrymarketBeApplication.java
+++ src/main/java/com/cherrydev/cherrymarketbe/CherrymarketBeApplication.java
@@ -3,7 +3,6 @@ package com.cherrydev.cherrymarketbe;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
@@ -11,7 +10,6 @@ import java.util.TimeZone;
 import static com.cherrydev.cherrymarketbe.server.application.common.log.CherryLogger.logServerStart;
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
 public class CherrymarketBeApplication {
 
--- src/main/java/com/cherrydev/cherrymarketbe/server/application/config/JpaConfig.java
+++ src/main/java/com/cherrydev/cherrymarketbe/server/application/config/JpaConfig.java
@@ -4,8 +4,10 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class JpaConfig {
 
     @Bean
