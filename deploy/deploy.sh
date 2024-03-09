95fbc12c45fd9406c8382ac9986dfb5dbb7a6f65
SeongHo5 <newfuring1123@gmail.com>
2024-03-09 20:09:49 +0900

    chore: Update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 REPOSITORY=~
-PROJECT_NAME=cherrymarket-be
+PROJECT_NAME=cherrymarket-server
+
+echo "> Git pull from remote repository"
+cd $REPOSITORY/$PROJECT_NAME || exit
+git pull
+
+echo "> Start build with Gradle"
+./gradlew build
 
 echo "> Copy build file"
 cp $REPOSITORY/$PROJECT_NAME/build/libs/*SNAPSHOT.jar $REPOSITORY/
@@ -11,7 +19,7 @@ echo "> Current running application PID: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
 	echo "> There is no running application, so it will not be terminated."
 else
-	echo "> kill -15 $CURRENT_PID"
+	echo "> kill -15 $CURRENT_PID and sleep 5"
 	kill -15 "$CURRENT_PID"
 	sleep 5
 fi
