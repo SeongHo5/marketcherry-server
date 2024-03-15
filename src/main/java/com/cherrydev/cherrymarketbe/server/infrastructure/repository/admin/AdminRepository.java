2cf93dd7ab831a6e04a431f979705346379ec82e
SeongHo5 <newfuring1123@gmail.com>
2024-03-15 18:16:36 +0900

    feat: Add admin repository and query
    - 관리자용 집계 메서드 추가

---
+++ src/main/java/com/cherrydev/cherrymarketbe/server/infrastructure/repository/admin/AdminRepository.java
@@ -0,0 +1,56 @@
+package com.cherrydev.cherrymarketbe.server.infrastructure.repository.admin;
+
+import com.cherrydev.cherrymarketbe.server.domain.goods.entity.Goods;
+import com.cherrydev.cherrymarketbe.server.domain.goods.entity.QGoods;
+import com.cherrydev.cherrymarketbe.server.domain.order.entity.QOrderDetail;
+import com.cherrydev.cherrymarketbe.server.domain.order.entity.QOrders;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class AdminRepository {
+
+    private static final QGoods qGoods = QGoods.goods;
+    private static final QOrders qOrders = QOrders.orders;
+    private static final QOrderDetail qOrderDetail = QOrderDetail.orderDetail;
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    // 특정연도, 월에 N번 이상 주문된 상품 목록
+    public Page<Goods> findGoodsByYearAndMonth(Pageable pageable, int year, int month, int count) {
+        List<Goods> goodsList = jpaQueryFactory
+                .select(qGoods)
+                .from(qOrders)
+                .join(qOrders.orderDetails, qOrderDetail)
+                .join(qOrderDetail.goods, qGoods)
+                .where(qOrders.createdAt.year().eq(year)
+                        .and(qOrders.createdAt.month().eq(month)))
+                .groupBy(qGoods)
+                .having(qOrderDetail.count().goe(count))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long totalCount = jpaQueryFactory
+                .select(qGoods.count())
+                .from(qOrders)
+                .join(qOrders.orderDetails, qOrderDetail)
+                .join(qOrderDetail.goods, qGoods)
+                .where(qOrders.createdAt.year().eq(year)
+                        .and(qOrders.createdAt.month().eq(month)))
+                .groupBy(qGoods)
+                .having(qOrderDetail.count().goe(count))
+                .fetchFirst();
+
+        return new PageImpl<>(goodsList, pageable, totalCount);
+    }
+
+
+}
