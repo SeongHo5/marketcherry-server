fc4f640f3408297311e051fe899aa4ee8855d502
SeongHo5 <newfuring1123@gmail.com>
2024-03-15 11:23:09 +0900

    refactor: Remove unused methods

--- src/main/java/com/cherrydev/cherrymarketbe/server/infrastructure/repository/account/AccountRepository.java
+++ src/main/java/com/cherrydev/cherrymarketbe/server/infrastructure/repository/account/AccountRepository.java
@@ -2,8 +2,6 @@ package com.cherrydev.cherrymarketbe.server.infrastructure.repository.account;
 
 import com.cherrydev.cherrymarketbe.server.domain.account.entity.Account;
 import com.cherrydev.cherrymarketbe.server.domain.account.enums.RegisterType;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
 
@@ -12,8 +10,6 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findByEmail(String email);
-
-    Page<Account> findAll(Pageable pageable);
 
     RegisterType getRegisterTypeByEmail(String email);
 
