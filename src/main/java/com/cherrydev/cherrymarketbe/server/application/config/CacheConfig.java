8f35e53340db906c5f00d594b0eb5811d24d9eab
SeongHo5 <newfuring1123@gmail.com>
2024-03-25 12:02:56 +0900

    refactor: Refactor CacheConfig

--- src/main/java/com/cherrydev/cherrymarketbe/server/application/config/CacheConfig.java
+++ src/main/java/com/cherrydev/cherrymarketbe/server/application/config/CacheConfig.java
@@ -1,6 +1,5 @@
 package com.cherrydev.cherrymarketbe.server.application.config;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,34 +18,47 @@ import java.time.Duration;
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        // 키 직렬화 설정
-        RedisSerializationContext.SerializationPair<String> keySerialization =
-                RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer());
-
-        // 값 직렬화 설정
-        RedisSerializationContext.SerializationPair<Object> valueSerialization =
-                RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
-
-        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(30))
-                .serializeKeysWith(keySerialization)
-                .serializeValuesWith(valueSerialization)
-                .disableCachingNullValues();
-
-        RedisCacheConfiguration accountCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
-                .serializeKeysWith(keySerialization)
-                .serializeValuesWith(valueSerialization)
-                .disableCachingNullValues();
-
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         return RedisCacheManager.builder()
                 .transactionAware()
                 .cacheWriter(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
-                .withCacheConfiguration("accountCache", accountCacheConfig)
-                .cacheDefaults(defaultCacheConfig)
+                .withCacheConfiguration("accountCache", accountCacheConfig())
+                .cacheDefaults(defaultCacheConfig())
                 .build();
     }
 
+    /**
+     * Key 직렬화 설정
+     */
+    @Bean
+    protected RedisSerializationContext.SerializationPair<String> keySerialization() {
+        return RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer());
+    }
+
+    /**
+     * Value 직렬화 설정
+     */
+    @Bean
+    protected RedisSerializationContext.SerializationPair<Object> valueSerialization() {
+        return RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+    }
+
+    @Bean
+    protected RedisCacheConfiguration defaultCacheConfig() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30))
+                .serializeKeysWith(keySerialization())
+                .serializeValuesWith(valueSerialization())
+                .disableCachingNullValues();
+    }
+
+    @Bean
+    protected RedisCacheConfiguration accountCacheConfig() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .serializeKeysWith(keySerialization())
+                .serializeValuesWith(valueSerialization())
+                .disableCachingNullValues();
+    }
 
 }
