50a5cf222a18490550ad6363b6dad16599e7dd26
SeongHo5 <newfuring1123@gmail.com>
2024-03-27 17:46:04 +0900

    docs: Add javadoc to DistributedLock

--- src/main/java/com/cherrydev/cherrymarketbe/server/application/annotation/DistributedLock.java
+++ src/main/java/com/cherrydev/cherrymarketbe/server/application/annotation/DistributedLock.java
@@ -1,11 +1,17 @@
 package com.cherrydev.cherrymarketbe.server.application.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 분산 락을 적용하기 위한 어노테이션
+ * <p>
+ *     - keyName: 락을 걸 key의 이름<br>
+ *     - waitTime: 락을 얻기 위해 대기하는 시간<br>
+ *     - leaseTime: 락을 유지하는 시간<br>
+ *     - timeUnit: 시간 단위
+ */
+@Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DistributedLock {
