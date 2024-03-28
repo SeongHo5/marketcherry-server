37f524c2ca646fda698759ec4d655e85e01f3030
SeongHo5 <newfuring1123@gmail.com>
2024-03-28 10:00:59 +0900

    refactor: Remove unnecessary annotation
    - Rollback 어노테이션 제거

--- src/test/java/com/cherrydev/cherrymarketbe/integration/IntegrationTest.java
+++ src/test/java/com/cherrydev/cherrymarketbe/integration/IntegrationTest.java
@@ -3,11 +3,9 @@ package com.cherrydev.cherrymarketbe.integration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.transaction.annotation.Transactional;
 
-@Rollback
 @Transactional
 @SpringBootTest
 @DisabledInAotMode
