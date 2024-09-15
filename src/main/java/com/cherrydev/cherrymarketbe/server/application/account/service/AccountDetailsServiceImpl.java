3f08f93017318566fb98de5f6cb6ea492817b8ba
SeongHo5 <newfuring1123@gmail.com>
2024-09-15 21:23:22 +0900

    style: Apply spotless

--- src/main/java/com/cherrydev/cherrymarketbe/server/application/account/service/AccountDetailsServiceImpl.java
+++ src/main/java/com/cherrydev/cherrymarketbe/server/application/account/service/AccountDetailsServiceImpl.java
@@ -1,25 +1,22 @@
 package com.cherrydev.cherrymarketbe.server.application.account.service;
 
+import com.cherrydev.cherrymarketbe.server.domain.account.dto.response.AccountDetails;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import lombok.RequiredArgsConstructor;
-
-import com.cherrydev.cherrymarketbe.server.domain.account.dto.response.AccountDetails;
-
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AccountDetailsServiceImpl implements UserDetailsService {
 
-    private final AccountQueryService accountQueryService;
+  private final AccountQueryService accountQueryService;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new AccountDetails(accountQueryService.fetchAccountEntity(username));
-    }
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return new AccountDetails(accountQueryService.fetchAccountEntity(username));
+  }
 }
